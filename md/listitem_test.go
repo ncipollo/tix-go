1af573b9f5f2d72fe36b517a5f54663ee953041d
Nick Cipollo <nick.cipollo@grubhub.com>
2020-01-25 15:41:37 -0500

    Add list parsing tests

--- md/listitem_test.go
+++ md/listitem_test.go
@@ -6,7 +6,7 @@ import (
 	"tix/ticket/body"
 )
 
-func TestListItemSegmentParser_Parse(t *testing.T) {
+func TestListItemSegmentParser_Parse_BulletList(t *testing.T) {
 	text := `
 - Item 1
 `
@@ -27,4 +27,42 @@ func TestListItemSegmentParser_Parse(t *testing.T) {
 	ticketBody := state.CurrentTicket().Body
 	assert.NoError(t, err)
 	assert.Equal(t, expectedBody, ticketBody)
+}
+
+func TestListItemSegmentParser_Parse_ErrorWhenNoList(t *testing.T) {
+	text := `
+- Item 1
+`
+	parser := NewListItemSegmentParser()
+	state, rootNode := setupTextParser(text)
+	state.StartTicket()
+	node := rootNode.FirstChild()
+
+	err := parser.Parse(state, node.FirstChild())
+
+	assert.Error(t, err)
+}
+
+func TestListItemSegmentParser_Parse_OrderedList(t *testing.T) {
+	text := `
+1. First
+`
+	parser := NewListItemSegmentParser()
+	state, rootNode := setupTextParser(text)
+	state.StartTicket()
+	state.ListState.StartOrderedList(1)
+	node := rootNode.FirstChild()
+
+	err := parser.Parse(state, node.FirstChild())
+
+	expectedBody := []body.Segment{
+		body.NewOrderedListItemSegment(1, 1),
+		body.NewLineBreakSegment(),
+		body.NewTextBlockSegment(),
+		body.NewTextSegment("First"),
+	}
+	ticketBody := state.CurrentTicket().Body
+	assert.NoError(t, err)
+	assert.Equal(t, expectedBody, ticketBody)
+	assert.Equal(t, 2, state.ListState.CurrentList().CurrentNumber)
 }
