cdbd3e88e164281af176fa05c3e210bdce7cea51
Nick Cipollo <nick.cipollo@grubhub.com>
2020-01-29 16:49:25 -0500

    Add jira api

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"github.com/yuin/goldmark"
-	"github.com/yuin/goldmark/ast"
-	"github.com/yuin/goldmark/text"
+	"github.com/andygrunwald/go-jira"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 const md = `
@@ -15,16 +16,92 @@ const md = `
 `
 
 func main() {
-	source := []byte(md)
-	reader := text.NewReader(source)
-	parser := goldmark.DefaultParser()
-	node := parser.Parse(reader)
-	ast.Walk(node, func(visited ast.Node, entering bool) (ast.WalkStatus, error) {
-		status := ast.WalkStatus(ast.WalkContinue)
-		kind := visited.Kind()
-		if entering {
-			println(kind.String())
+	envMap := make(map[string]string)
+	for _, env := range os.Environ() {
+		pair := strings.SplitN(env, "=", 2)
+		envMap[pair[0]] = pair[1]
+	}
+
+	userName := envMap["AGENCY_BOT_USERNAME"]
+	password := envMap["AGENCY_BOT_API_TOKEN"]
+
+	tp := jira.BasicAuthTransport{
+		Username: userName,
+		Password: password,
+	}
+
+	client, err := jira.NewClient(tp.Client(), "https://levelup.atlassian.net/")
+	if err != nil {
+		panic(err)
+	}
+
+	issue, _, err := client.Issue.Get("SDK-3047", nil)
+
+	fields, _, err := client.Field.GetList()
+	epicNameKey := ""
+	epicLinkKey := ""
+
+	for _, field := range fields {
+		if field.Name == "Epic Name" {
+			epicNameKey = field.ID
 		}
-		return status, nil
-	})
+		if field.Name == "Epic Link" {
+			epicLinkKey = field.ID
+		}
+		if field.ID == "customfield_13615" {
+			println("here")
+		}
+	}
+
+	i := jira.Issue{
+		Fields: &jira.IssueFields{
+			Components: []*jira.Component{
+				{Name: "Android SDK"},
+			},
+			Description: "Test Issue",
+			Labels:      []string{"higgs-pod"},
+			Type: jira.IssueType{
+				Name: "Epic",
+			},
+			Project: jira.Project{
+				Key: "SDK",
+			},
+			Summary: "Android:: Tix Generated Test Epic",
+			Unknowns: map[string]interface{}{
+				epicNameKey: "Nick Test",
+			},
+		},
+	}
+	epicIssue, response, err := client.Issue.Create(&i)
+
+	i = jira.Issue{
+		Fields: &jira.IssueFields{
+			Components: []*jira.Component{
+				{Name: "Android SDK"},
+			},
+			Description: "Test Issue",
+			Labels:      []string{"higgs-pod"},
+			Type: jira.IssueType{
+				Name: "Story",
+			},
+			Project: jira.Project{
+				Key: "SDK",
+			},
+			Summary: "Android:: Tix Generated Test Issue",
+			Unknowns: map[string]interface{}{
+				epicLinkKey: epicIssue.Key,
+				"customfield_13615": map[string]interface{}{
+					"value": "No design review required",
+				},
+			},
+		},
+	}
+
+	issue, response, err = client.Issue.Create(&i)
+
+	body, err := ioutil.ReadAll(response.Body)
+	bodyStr := string(body)
+	println(issue)
+	println(bodyStr)
+	println(fields)
 }
