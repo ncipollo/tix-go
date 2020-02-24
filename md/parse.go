9a0d71ff53483c0a40949519052d49f095750ee9
Nick Cipollo <nick.cipollo@grubhub.com>
2020-02-24 18:32:41 -0500

    Fixes #2 Remoove unused parsers

--- md/parse.go
+++ md/parse.go
@@ -1,9 +1,10 @@
 package md
 
 import (
-	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/ast"
+	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/text"
+	"github.com/yuin/goldmark/util"
 	"tix/ticket"
 )
 
@@ -35,10 +36,41 @@ func setupParser(source []byte, fieldState *FieldState) (*State, ast.Node) {
 	state := newState(source, fieldState)
 
 	reader := text.NewReader(source)
-	parser := goldmark.DefaultParser()
-	rootNode := parser.Parse(reader)
+	markdownParser := createMarkdownParser()
+	rootNode := markdownParser.Parse(reader)
 
 	return state, rootNode
+}
+
+func createMarkdownParser() parser.Parser {
+	return parser.NewParser(blockParsers(),
+		inlineParsers(),
+		parser.WithParagraphTransformers(parser.DefaultParagraphTransformers()...),
+	)
+}
+
+func blockParsers() parser.Option  {
+	parsers := []util.PrioritizedValue{
+		util.Prioritized(parser.NewSetextHeadingParser(), 100),
+		util.Prioritized(parser.NewThematicBreakParser(), 200),
+		util.Prioritized(parser.NewListParser(), 300),
+		util.Prioritized(parser.NewListItemParser(), 400),
+		util.Prioritized(parser.NewCodeBlockParser(), 500),
+		util.Prioritized(parser.NewATXHeadingParser(), 600),
+		util.Prioritized(parser.NewFencedCodeBlockParser(), 700),
+		util.Prioritized(parser.NewBlockquoteParser(), 800),
+		util.Prioritized(parser.NewParagraphParser(), 900),
+	}
+	return parser.WithBlockParsers(parsers...)
+}
+
+func inlineParsers() parser.Option  {
+	parsers := []util.PrioritizedValue{
+		util.Prioritized(parser.NewCodeSpanParser(), 100),
+		util.Prioritized(parser.NewLinkParser(), 200),
+		util.Prioritized(parser.NewEmphasisParser(), 300),
+	}
+	return parser.WithInlineParsers(parsers...)
 }
 
 func setupTextParser(text string) (*State, ast.Node) {
