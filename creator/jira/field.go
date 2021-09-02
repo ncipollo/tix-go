0e755d11136b6685d14f40038ca3793e1a172256
Nick Cipollo <njc1115@gmail.com>
2021-09-02 18:35:12 -0400

    Add support for epics in normal projects.

--- creator/jira/field.go
+++ creator/jira/field.go
@@ -1,197 +1,208 @@
 package jira
 
 import (
-	"github.com/andygrunwald/go-jira"
-	"strings"
-	"tix/ticket"
+    "github.com/andygrunwald/go-jira"
+    "strings"
+    "tix/ticket"
 )
 
 const (
-	KeyAffectsVersions = "affects versions"
-	KeyComponents      = "components"
-	KeyFixVersions     = "fix versions"
-	KeyLabels          = "labels"
-	KeyProject         = "project"
-	KeyType            = "type"
+    KeyAffectsVersions = "affects versions"
+    KeyComponents      = "components"
+    KeyFixVersions     = "fix versions"
+    KeyLabels          = "labels"
+    KeyProject         = "project"
+    KeyType            = "type"
+    KeyUseParent       = "use_parent"
 )
 
 type FieldInfo struct {
-	ID          string
-	Name        string
-	UseValueKey bool
+    ID          string
+    Name        string
+    UseValueKey bool
 }
 
 func fieldInfoByName(jiraFields []jira.Field) map[string]*FieldInfo {
-	fieldMap := make(map[string]*FieldInfo)
-	for _, jiraField := range jiraFields {
-		info := &FieldInfo{
-			ID:          jiraField.ID,
-			Name:        jiraField.Name,
-			UseValueKey: jiraField.Schema.Type == "option", // There may also be others
-		}
-		key := strings.ToLower(info.Name)
-		fieldMap[key] = info
-	}
-	return fieldMap
+    fieldMap := make(map[string]*FieldInfo)
+    for _, jiraField := range jiraFields {
+        info := &FieldInfo{
+            ID:          jiraField.ID,
+            Name:        jiraField.Name,
+            UseValueKey: jiraField.Schema.Type == "option", // There may also be others
+        }
+        key := strings.ToLower(info.Name)
+        fieldMap[key] = info
+    }
+    return fieldMap
 }
 
 type IssueFields struct {
-	ticket    *ticket.Ticket
-	fieldInfo map[string]*FieldInfo
+    ticket    *ticket.Ticket
+    fieldInfo map[string]*FieldInfo
 }
 
 func NewIssueFields(jiraFields []jira.Field, ticket *ticket.Ticket) *IssueFields {
-	return &IssueFields{
-		ticket:    ticket,
-		fieldInfo: fieldInfoByName(jiraFields),
-	}
+    return &IssueFields{
+        ticket:    ticket,
+        fieldInfo: fieldInfoByName(jiraFields),
+    }
 }
 
 func (i *IssueFields) AddDefaultEpicName(unknowns map[string]interface{}, name string) {
-	epicNameKey := i.fieldInfo["epic name"].ID
-	epicName, ok := unknowns[epicNameKey].(string)
-	if !ok || len(epicName) == 0 {
-		unknowns[epicNameKey] = name
-	}
+    epicNameKey := i.fieldInfo["epic name"].ID
+    epicName, ok := unknowns[epicNameKey].(string)
+    if !ok || len(epicName) == 0 {
+        unknowns[epicNameKey] = name
+    }
 }
 
 func (i *IssueFields) AffectsVersions() []*jira.AffectsVersion {
-	rawVersions, ok := i.ticket.Fields("jira")[KeyAffectsVersions].([]interface{})
-	if ok {
-		versions := make([]*jira.AffectsVersion, 0)
-		for _, label := range rawVersions {
-			versionString, ok := label.(string)
-			if ok {
-				versions = append(versions, &jira.AffectsVersion{Name: versionString})
-			}
-		}
-		return versions
-	} else {
-		return nil
-	}
+    rawVersions, ok := i.ticket.Fields("jira")[KeyAffectsVersions].([]interface{})
+    if ok {
+        versions := make([]*jira.AffectsVersion, 0)
+        for _, label := range rawVersions {
+            versionString, ok := label.(string)
+            if ok {
+                versions = append(versions, &jira.AffectsVersion{Name: versionString})
+            }
+        }
+        return versions
+    } else {
+        return nil
+    }
 }
 
 func (i *IssueFields) Components() []*jira.Component {
-	components, ok := i.ticket.Fields("jira")[KeyComponents].([]interface{})
-	if !ok {
-		return nil
-	}
-	jiraComps := make([]*jira.Component, 0)
-	for _, component := range components {
-		compString, ok := component.(string)
-		if !ok {
-			compString = ""
-		}
-		jiraComp := &jira.Component{Name: compString}
-		jiraComps = append(jiraComps, jiraComp)
-	}
-	return jiraComps
+    components, ok := i.ticket.Fields("jira")[KeyComponents].([]interface{})
+    if !ok {
+        return nil
+    }
+    jiraComps := make([]*jira.Component, 0)
+    for _, component := range components {
+        compString, ok := component.(string)
+        if !ok {
+            compString = ""
+        }
+        jiraComp := &jira.Component{Name: compString}
+        jiraComps = append(jiraComps, jiraComp)
+    }
+    return jiraComps
 }
 
 func (i *IssueFields) EpicLinkKey() string {
-	return i.fieldInfo["epic link"].ID
+    return i.fieldInfo["epic link"].ID
 }
 
 func (i *IssueFields) EpicType() jira.IssueType {
-	issueType, ok := i.ticket.Fields("jira")[KeyType].(string)
-	if !ok {
-		issueType = "Epic"
-	}
-	return jira.IssueType{
-		Name: issueType,
-	}
+    issueType, ok := i.ticket.Fields("jira")[KeyType].(string)
+    if !ok {
+        issueType = "Epic"
+    }
+    return jira.IssueType{
+        Name: issueType,
+    }
+}
+
+func (i *IssueFields) UseParent() bool {
+    useParent, ok := i.ticket.Fields("jira")[KeyUseParent].(bool)
+    if ok {
+        return useParent
+    } else {
+        return false
+    }
 }
 
 func (i *IssueFields) FixVersions() []*jira.FixVersion {
-	rawVersions, ok := i.ticket.Fields("jira")[KeyFixVersions].([]interface{})
-	if ok {
-		versions := make([]*jira.FixVersion, 0)
-		for _, label := range rawVersions {
-			versionString, ok := label.(string)
-			if ok {
-				versions = append(versions, &jira.FixVersion{Name: versionString})
-			}
-		}
-		return versions
-	} else {
-		return nil
-	}
+    rawVersions, ok := i.ticket.Fields("jira")[KeyFixVersions].([]interface{})
+    if ok {
+        versions := make([]*jira.FixVersion, 0)
+        for _, label := range rawVersions {
+            versionString, ok := label.(string)
+            if ok {
+                versions = append(versions, &jira.FixVersion{Name: versionString})
+            }
+        }
+        return versions
+    } else {
+        return nil
+    }
 }
 
 func (i *IssueFields) Labels() []string {
-	labels, ok := i.ticket.Fields("jira")[KeyLabels].([]interface{})
-	if ok {
-		labelStrings := make([]string, 0)
-		for _, label := range labels {
-			labelString, ok := label.(string)
-			if ok {
-				labelStrings = append(labelStrings, labelString)
-			}
-		}
-		return labelStrings
-	} else {
-		return make([]string, 0)
-	}
+    labels, ok := i.ticket.Fields("jira")[KeyLabels].([]interface{})
+    if ok {
+        labelStrings := make([]string, 0)
+        for _, label := range labels {
+            labelString, ok := label.(string)
+            if ok {
+                labelStrings = append(labelStrings, labelString)
+            }
+        }
+        return labelStrings
+    } else {
+        return make([]string, 0)
+    }
 }
 
 func (i *IssueFields) IssueType() jira.IssueType {
-	issueType, ok := i.ticket.Fields("jira")[KeyType].(string)
-	if !ok {
-		issueType = "Story"
-	}
-	return jira.IssueType{
-		Name: issueType,
-	}
+    issueType, ok := i.ticket.Fields("jira")[KeyType].(string)
+    if !ok {
+        issueType = "Story"
+    }
+    return jira.IssueType{
+        Name: issueType,
+    }
 }
 
 func (i *IssueFields) Project() jira.Project {
-	project, ok := i.ticket.Fields("jira")[KeyProject].(string)
-	if !ok {
-		project = ""
-	}
-	return jira.Project{
-		Key: project,
-	}
+    project, ok := i.ticket.Fields("jira")[KeyProject].(string)
+    if !ok {
+        project = ""
+    }
+    return jira.Project{
+        Key: project,
+    }
 }
 
 func (i *IssueFields) TaskType() jira.IssueType {
-	issueType, ok := i.ticket.Fields("jira")[KeyType].(string)
-	if !ok {
-		issueType = "Task"
-	}
-	return jira.IssueType{
-		Name: issueType,
-	}
+    issueType, ok := i.ticket.Fields("jira")[KeyType].(string)
+    if !ok {
+        issueType = "Task"
+    }
+    return jira.IssueType{
+        Name: issueType,
+    }
 }
 
 func (i *IssueFields) Unknowns() map[string]interface{} {
-	keysToSkip := map[string]bool{
-		KeyAffectsVersions:     true,
-		KeyComponents:          true,
-		KeyFixVersions:         true,
-		KeyLabels:              true,
-		KeyProject:             true,
-		KeyType:                true,
-		ticket.KeyUpdateTicket: true,
-	}
+    keysToSkip := map[string]bool{
+        KeyAffectsVersions:     true,
+        KeyComponents:          true,
+        KeyFixVersions:         true,
+        KeyLabels:              true,
+        KeyProject:             true,
+        KeyType:                true,
+        ticket.KeyUpdateTicket: true,
+        KeyUseParent:           true,
+    }
 
-	unknown := make(map[string]interface{})
-	for key, field := range i.ticket.Fields("jira") {
-		lowerKey := strings.ToLower(key)
-		if keysToSkip[lowerKey] {
-			continue
-		}
+    unknown := make(map[string]interface{})
+    for key, field := range i.ticket.Fields("jira") {
+        lowerKey := strings.ToLower(key)
+        if keysToSkip[lowerKey] {
+            continue
+        }
 
-		info := i.fieldInfo[lowerKey]
-		if info != nil {
-			if info.UseValueKey {
-				unknown[info.ID] = map[string]interface{}{"value": field}
-			} else {
-				unknown[info.ID] = field
-			}
-		} else {
-			unknown[key] = field
-		}
-	}
-	return unknown
+        info := i.fieldInfo[lowerKey]
+        if info != nil {
+            if info.UseValueKey {
+                unknown[info.ID] = map[string]interface{}{"value": field}
+            } else {
+                unknown[info.ID] = field
+            }
+        } else {
+            unknown[key] = field
+        }
+    }
+    return unknown
 }
--- creator/jira/issues.go
+++ creator/jira/issues.go
@@ -1,97 +1,102 @@
 package jira
 
 import (
-	"github.com/andygrunwald/go-jira"
-	"tix/render"
-	"tix/ticket"
+    "github.com/andygrunwald/go-jira"
+    "tix/render"
+    "tix/ticket"
 )
 
 type Issues struct {
-	jiraFields []jira.Field
-	renderer   render.BodyRenderer
+    jiraFields []jira.Field
+    renderer   render.BodyRenderer
 }
 
 func NewIssues(jiraFields []jira.Field, renderer render.BodyRenderer) *Issues {
-	return &Issues{jiraFields: jiraFields, renderer: renderer}
+    return &Issues{jiraFields: jiraFields, renderer: renderer}
 }
 
 func (i *Issues) FromTicket(ticket *ticket.Ticket, parentIssue *jira.Issue, ticketLevel int) *jira.Issue {
-	switch ticketLevel {
-	case 0:
-		return i.epic(ticket)
-	case 1:
-		return i.story(ticket, parentIssue)
-	default:
-		return i.task(ticket, parentIssue)
-	}
+    switch ticketLevel {
+    case 0:
+        return i.epic(ticket)
+    case 1:
+        return i.story(ticket, parentIssue)
+    default:
+        return i.task(ticket, parentIssue)
+    }
 }
 
 func (i *Issues) epic(ticket *ticket.Ticket) *jira.Issue {
-	description := i.renderBody(ticket)
-	issueFields := NewIssueFields(i.jiraFields, ticket)
-	// Add epic name if missing
-	unknowns := issueFields.Unknowns()
-	issueFields.AddDefaultEpicName(unknowns, ticket.Title)
+    description := i.renderBody(ticket)
+    issueFields := NewIssueFields(i.jiraFields, ticket)
+    // Add epic name if missing
+    unknowns := issueFields.Unknowns()
 
-	return &jira.Issue{
-		Fields: &jira.IssueFields{
-			AffectsVersions: issueFields.AffectsVersions(),
-			Components:      issueFields.Components(),
-			Description:     description,
-			FixVersions: issueFields.FixVersions(),
-			Labels:          issueFields.Labels(),
-			Type:            issueFields.EpicType(),
-			Project:         issueFields.Project(),
-			Summary:         ticket.Title,
-			Unknowns:        unknowns,
-		},
-	}
+    return &jira.Issue{
+        Fields: &jira.IssueFields{
+            AffectsVersions: issueFields.AffectsVersions(),
+            Components:      issueFields.Components(),
+            Description:     description,
+            FixVersions:     issueFields.FixVersions(),
+            Labels:          issueFields.Labels(),
+            Type:            issueFields.EpicType(),
+            Project:         issueFields.Project(),
+            Summary:         ticket.Title,
+            Unknowns:        unknowns,
+        },
+    }
 }
 
 func (i *Issues) story(ticket *ticket.Ticket, parentIssue *jira.Issue) *jira.Issue {
-	description := i.renderBody(ticket)
-	issueFields := NewIssueFields(i.jiraFields, ticket)
-	unknowns := issueFields.Unknowns()
-	if parentIssue != nil {
-		// Add epic link to unknowns
-		unknowns[issueFields.EpicLinkKey()] = parentIssue.Key
-	}
+    description := i.renderBody(ticket)
+    issueFields := NewIssueFields(i.jiraFields, ticket)
+    unknowns := issueFields.Unknowns()
+	var parent *jira.Parent
+    if parentIssue != nil {
+        if issueFields.UseParent() {
+            parent = &jira.Parent{Key: parentIssue.Key}
+        } else  {
+            // Add epic link to unknowns
+            unknowns[issueFields.EpicLinkKey()] = parentIssue.Key
+        }
+    }
 
-	return &jira.Issue{
-		Fields: &jira.IssueFields{
-			AffectsVersions: issueFields.AffectsVersions(),
-			Components:  issueFields.Components(),
-			Description: description,
-			FixVersions: issueFields.FixVersions(),
-			Labels:      issueFields.Labels(),
-			Type:        issueFields.IssueType(),
-			Project:     issueFields.Project(),
-			Summary:     ticket.Title,
-			Unknowns:    unknowns,
-		},
-	}
+    return &jira.Issue{
+        Fields: &jira.IssueFields{
+            AffectsVersions: issueFields.AffectsVersions(),
+            Components:      issueFields.Components(),
+            Description:     description,
+            FixVersions:     issueFields.FixVersions(),
+            Labels:          issueFields.Labels(),
+            Type:            issueFields.IssueType(),
+            Parent:          parent,
+            Project:         issueFields.Project(),
+            Summary:         ticket.Title,
+            Unknowns:        unknowns,
+        },
+    }
 }
 
 func (i *Issues) task(ticket *ticket.Ticket, parentIssue *jira.Issue) *jira.Issue {
-	description := i.renderBody(ticket)
-	issueFields := NewIssueFields(i.jiraFields, ticket)
+    description := i.renderBody(ticket)
+    issueFields := NewIssueFields(i.jiraFields, ticket)
 
-	return &jira.Issue{
-		Fields: &jira.IssueFields{
-			AffectsVersions: issueFields.AffectsVersions(),
-			Components:  issueFields.Components(),
-			Description: description,
-			FixVersions: issueFields.FixVersions(),
-			Labels:      issueFields.Labels(),
-			Type:        issueFields.TaskType(),
-			Project:     issueFields.Project(),
-			Parent:      &jira.Parent{ID: parentIssue.ID},
-			Summary:     ticket.Title,
-			Unknowns:    issueFields.Unknowns(),
-		},
-	}
+    return &jira.Issue{
+        Fields: &jira.IssueFields{
+            AffectsVersions: issueFields.AffectsVersions(),
+            Components:      issueFields.Components(),
+            Description:     description,
+            FixVersions:     issueFields.FixVersions(),
+            Labels:          issueFields.Labels(),
+            Type:            issueFields.TaskType(),
+            Project:         issueFields.Project(),
+            Parent:          &jira.Parent{ID: parentIssue.ID},
+            Summary:         ticket.Title,
+            Unknowns:        issueFields.Unknowns(),
+        },
+    }
 }
 
 func (i *Issues) renderBody(ticket *ticket.Ticket) string {
-	return render.Ticket(ticket, i.renderer)
+    return render.Ticket(ticket, i.renderer)
 }
